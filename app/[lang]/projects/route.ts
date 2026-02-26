885f17dee1c0f434c801a19085050104be7d9055
Nooc <nooc@nooc.me>
2026-02-26 15:48:48 +0800

    Add edge runtime to projects route

--- app/[lang]/projects/route.ts
+++ app/[lang]/projects/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 
+export const runtime = "edge";
+
 export function GET(req: Request) {
   const url = new URL(req.url);
   const newUrl = new URL("/works", url.origin);
